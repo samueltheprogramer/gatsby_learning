4555cb9d516ae740c99d3ef710401e99ca44e977
Samuel . V <samuelpocom3@gmail.com>
2024-08-18 20:02:01 +0530

    deploy

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <div className="w-full px-10 text-center">
+      <h1>Hello world !!!</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
